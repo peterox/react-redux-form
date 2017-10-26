5651ba216f8b6dcd9d473de35696d0a4e723530e
Peter Oxenham <peterox@users.noreply.github.com>
2017-10-26 11:50:13 +1000

    Revert "Ensuring that flushing does not occur more than once. Fixes #951 #884"
    This reverts commit dd333e56251bb34d69fbd130aaabc684870bfbb3.

--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -5,9 +5,6 @@ export default function debounce(func, delay) {
   const createLaterFunc = (args) => () => {
     timeout = null;
     func.apply(null, args);
-
-    // Only run the deferred function once
-    laterFunc = undefined;
   };
 
   const debouncedFunc = (...args) => {
