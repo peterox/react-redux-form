482c2163e15451fd1e81be40cd44019bd5dcbc27
Peter Oxenham <peter@divvito.com>
2017-10-26 12:15:29 +1000

    Issue #792 Fix for react native

--- src/native.js
+++ src/native.js
@@ -12,24 +12,29 @@ import {
   View,
 } from 'react-native';
 
-import {
-  modelReducer,
-  formReducer,
-  modeled,
-  actions,
-  combineForms,
-  initialFieldState,
-  actionTypes,
-  Control,
-  Form,
-  Fieldset,
-  Errors,
-  batched,
-  form,
-  getField,
-  track,
-} from './index';
+
+import actions from './actions';
+import actionTypes from './action-types';
+import Fieldset from './components/fieldset-component';
+import Form from './components/form-component';
+import Errors from './components/errors-component';
+import modeled from './enhancers/modeled-enhancer';
+import batched from './enhancers/batched-enhancer';
+
+import formReducer from './reducers/form-reducer';
+import initialFieldState from './constants/initial-field-state';
+import combineForms from './reducers/forms-reducer';
+
+import modelReducer from './reducers/model-reducer';
+
+import track from './utils/track';
+
+import form from './form';
+
 import omit from './utils/omit';
+import _get from './utils/get';
+import getFieldFromState from './utils/get-field-from-state';
+import createControlClass from './components/control-component-factory';
 
 function getTextValue(value) {
   if (typeof value === 'string' || typeof value === 'number') {
@@ -40,6 +45,12 @@ function getTextValue(value) {
 }
 
 const noop = () => undefined;
+
+const Control = createControlClass({
+  get: _get,
+  getFieldFromState,
+  actions,
+});
 
 Control.MapView = (props) => (
   <Control
@@ -183,6 +194,6 @@ export {
   form,
 
   // Utilities
-  getField,
+  getFieldFromState as getField,
   track,
 };
